d95af1d163a92b6b1728a1bf34786bab90f3556b
Cantlin Ashrowan <cantlin@ashrowan.com>
2011-04-21 15:22:57 +0100

    rm random print_r from connector

--- assets/components/shepherd/connector.php
+++ assets/components/shepherd/connector.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
 require_once MODX_CONNECTORS_PATH.'index.php';
@@ -9,7 +10,7 @@ require_once $corePath.'model/shepherd/shepherd.class.php';
 $modx->shepherd = new Shepherd($modx);
 
 $modx->lexicon->load('shepherd:default');
-print_r($modx->shepherd->config); 
+
 /* handle request */
 $path = $modx->getOption('processorsPath',$modx->shepherd->config,$corePath.'processors/');
 $modx->request->handleRequest(array(
